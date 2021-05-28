3193b3b345f073eacfb2adecc361f0ad0b04c040
Stefan Kondinski <kondinskis@gmail.com>
2021-05-28 14:21:41 +0200

    adds requirements.txt

--- survey/resources/auth.py
+++ survey/resources/auth.py
@@ -4,7 +4,7 @@ from werkzeug.exceptions import Unauthorized
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
-    jwt_refresh_token_required,
+    jwt_required,
     current_user,
 )
 
@@ -14,7 +14,7 @@ from survey.schemas.auth import token_request_schema, token_schema
 from survey.extensions import jwt
 
 
-@jwt.user_claims_loader
+@jwt.additional_claims_loader
 def add_claims_to_access_token(user):
     return {"role": user.role.name}
 
@@ -24,7 +24,7 @@ def user_identity_lookup(user):
     return user.email
 
 
-@jwt.user_loader_callback_loader
+@jwt.user_lookup_loader
 def user_loader_callback(identity):
     user = UserService.get_by_email(identity)
     return user
@@ -54,7 +54,7 @@ class AccessToken(Resource):
 @ns.route("/refresh")
 class RefreshToken(Resource):
     @ns.marshal_with(token_schema, skip_none=True)
-    @jwt_refresh_token_required
+    @jwt_required(refresh=True)
     def post(self):
         ret = {"access_token": create_access_token(identity=current_user)}
         return ret
