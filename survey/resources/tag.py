4af2ce5d2982cfbc5fc8092b25a7bbd186a80ef9
Stefan Kondinski <kondinskis@gmail.com>
2021-05-28 15:45:03 +0200

    flask-jwt-extended changes

--- survey/resources/tag.py
+++ survey/resources/tag.py
@@ -11,17 +11,17 @@ from survey.schemas.tag import schema
 class Tag(Resource):
     @ns.marshal_with(schema, skip_none=True)
     @ns.expect(schema)
-    @jwt_required
+    @jwt_required()
     def put(self, id):
         tag = TagService.update(id, ns.payload)
         return tag
 
-    @jwt_required
+    @jwt_required()
     def delete(self, id):
         TagService.delete(id), 204
 
     @ns.marshal_with(schema, skip_none=True)
-    @jwt_required
+    @jwt_required()
     def get(self, id):
         tag = TagService.get(id)
         return tag
@@ -30,14 +30,14 @@ class Tag(Resource):
 @ns.route("")
 class TagList(Resource):
     @ns.marshal_list_with(schema, skip_none=True)
-    @jwt_required
+    @jwt_required()
     def get(self):
         tags = TagService.get_all()
         return tags
 
     @ns.marshal_with(schema, skip_none=True)
     @ns.expect(schema)
-    @jwt_required
+    @jwt_required()
     def post(self):
         tag = TagService.create(ns.payload)
         return tag, 201
