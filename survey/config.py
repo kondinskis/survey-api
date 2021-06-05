c8b18dd9245a2c58369dce12f69eda8d9bc7b12c
Stefan Kondinski <kondinskis@gmail.com>
2021-06-05 22:28:08 +0200

    fixes issue with heroku env variables

--- survey/config.py
+++ survey/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     JWT_SECRET_KEY = os.getenv("SECRET_KEY")
